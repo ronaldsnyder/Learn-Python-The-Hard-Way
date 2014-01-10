3bd1b6bd226f87830ba6995e2d3149eac5f2951c
rsnyder <rsnyder@rsnyder-Ubuntu.(none)>
2014-01-09 21:00:42 -0500

    chapter 13

---
+++ chapter13/ex13b.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+name = raw_input("enter_name ")
+print "This script is called:", script
+print "Hi ", name
+print "Your second variable is:", second
+print "Your third variable is ", third
