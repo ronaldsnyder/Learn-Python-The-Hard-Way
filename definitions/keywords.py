d418196e55c0a4614eb26001b68f53c6c090182a
ronaldsnyder <ronalddsnyder@gmail.com>
2014-01-18 13:01:59 -0500

    added keywords and chapter38 examples

---
+++ definitions/keywords.py
@@ -0,0 +1,32 @@
+#and - used in boolean
+#as - give a module a different name
+#assert - debugging tool
+#break - stop loop cycle if needed
+#class - create an object
+#continue - stops loop and starts over from the beginning
+#def - creates new user defined function ex def myName(ron):
+#del - deletes an object
+#elif - runs code block if condition is met
+#else - runs code block if condition is not met
+#except - what executes when there is an exception.  Used with try
+#exec - executes code dynamically
+#finally - code that is always executed
+#for - iteration loop 
+#from - import something specific from a module
+#gobal - access variable from outside function
+#if - runs code block if condition is met
+#import - gets other modules to use within your program
+#in
+#is - test for object identity
+#lambda - creates a new anonymous function
+#not - negates boolean
+#or - one condition must be met
+#pass - does nothing, use when you want to run a program but haven't created definitions of methods or objects.
+#print - prints to console
+#raise - a user defined exception
+#return - exits function and gives value back to what called the function
+#try - used in exception handling
+#while - runs loop while condition is met
+#with
+#yield - used with generators
+
