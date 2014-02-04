702528dee03c19aa0465e7656abbfea313a1697f
ronaldsnyder <ronalddsnyder@gmail.com>
2014-02-03 20:11:19 -0500

    chapter 46, project skeleton

---
+++ chapter46/projects/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+config = {
+          'description' : 'My Project',
+          'author' : 'Ron Snyder',
+          'url' : 'complete me',
+          'download_url' : 'complete me',
+          'author_email' : 'ronalddsnyder@gmail.com',
+          'version' : '0.1',
+          'install_requires': 'nose',
+          'packages' : 'NAME',
+          'scripts' : [],
+          'name' : 'projectname'
+}
+
+setup(**config)
